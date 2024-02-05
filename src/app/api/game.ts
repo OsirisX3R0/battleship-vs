bb9e68a9771016e9b13973d5bea1b2ffa7e56402
Richard Harris <richardharris916@gmail.com>
2024-02-04 21:54:31 -0600

    Initial routes

--- src/app/api/game.ts
+++ src/app/api/game.ts
@@ -1,5 +1,28 @@
+import prisma from '@/core/prisma'
 import { type NextRequest } from 'next/server'
- 
-export function POST(request: NextRequest) {
-  
+
+type GameRequestParams = {
+  id?: number,
+  code?: string,
+  players?: 1 | 2
+}
+
+type DeleteGameRequest = {
+  params: GameRequestParams
+}
+
+export async function POST(request: Request) {
+  let { code } = await request.json()
+  return await prisma.games.create({
+    data: {
+      code,
+      players: 1
+    } 
+  })
+}
+
+export async function DELETE(request: Request, {params}: DeleteGameRequest) {
+  return await prisma.games.delete({
+    where: { id: params.id }
+  })
 }
